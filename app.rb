441992038f89c738a52f9099b6f4a2d59f43957c
Yassir Mabrouk <yashiruraion@gmail.com>
2025-02-18 21:25:42 +0100

    Change refresh_interval from command line

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'net/http'
 class Url
   def initialize(address)
     @uri = URI(address)
+    raise NotValidUrl.new(address) unless @uri.kind_of?(URI::HTTP)
   end
 
   def path
@@ -19,9 +20,22 @@ class Url
   end
 end
 
+class NotValidUrl < StandardError
+  def initialize(address)
+    super("Url '#{address}' is not valid")
+  end
+end
+
+class MissingUrl < StandardError
+  def initialize
+    super('Url is missing')
+  end
+end
+
 class App
-  def initialize(url)
+  def initialize(url, refresh_interval)
     @url = url
+    @refresh_interval = refresh_interval
   end
 
   def start
@@ -31,10 +45,10 @@ class App
     loop do
       res = req.get @url.path
       puts "#{@url} -> #{res.code} #{res.message}"
-      sleep 2
+      sleep @refresh_interval
     rescue Errno::ECONNREFUSED
       puts "#{@url} -> Connection refused"
-      sleep 2
+      sleep @refresh_interval
     end
 
   rescue Interrupt
@@ -42,9 +56,34 @@ class App
   end
 end
 
-if ARGV.size == 1
-  url = Url.new(ARGV[0])
-  App.new(url).start
-else
-  puts "ruby #{$0} [url]"
+class Arg
+  attr_reader :refresh_interval
+
+  def initialize(argv)
+    @argv = argv
+    @refresh_interval = parse_refresh_interval
+  end
+
+  def url
+    raise MissingUrl.new unless @argv[0]
+    Url.new(@argv[0])
+  end
+
+  private
+  def parse_refresh_interval
+    arg_index = @argv.find_index { |arg| arg.start_with?('--refresh_interval=') }
+    return 2 unless arg_index
+    arg_value = @argv[arg_index].split('=')[1]
+    return 2 unless arg_value
+    @argv.delete_at(arg_index)
+    arg_value.to_i
+  end
+end
+
+begin
+  arg = Arg.new(ARGV)
+  App.new(arg.url, arg.refresh_interval).start
+rescue => ex
+  puts "ERROR : #{ex.message}"
+  puts "ruby #{$0} [url] --refresh_interval=[refresh_interval]"
 end
