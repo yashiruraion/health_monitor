77495a32dfbe88cb1bff52b522be115b441bbcfe
Yassir Mabrouk <yashiruraion@gmail.com>
2025-02-20 12:43:44 +0100

    Add color to printed text

--- app.rb
+++ app.rb
@@ -1,97 +1,125 @@
 require 'net/http'
 
 module HealthMonitor
-  class UrlNotValid < StandardError
-    def initialize
-      super
+  module Exceptions
+    class UrlNotValid < StandardError
+      def initialize
+        super
+      end
+    end
+
+    class UrlNotFound < StandardError
+      def initialize
+        super
+      end
     end
   end
 
-  class UrlNotFound < StandardError
-    def initialize
-      super
-    end
-  end
-end
-
-class App
-  def initialize(url, refresh_interval)
-    @url = url
-    @refresh_interval = refresh_interval
-  end
-
-  def start
-    req = Net::HTTP.new(@url.host, @url.port)
-    req.use_ssl = true if @url.secure?
-    req.open_timeout = 5
-
-    loop do
-      res = req.get @url.path
-      puts "#{@url} -> #{res.code} #{res.message}"
-      sleep @refresh_interval
-    rescue Errno::ECONNREFUSED
-      puts "#{@url} -> Connection refused"
-      sleep @refresh_interval
+  module Print
+    def self.normal(text='')
+      puts text
     end
 
-  rescue Interrupt
-    puts
-  end
-end
+    def self.green(text='')
+      puts "\e[38;2;0;255;0m#{text}\e[0m"
+    end
 
-class Url
-  attr_reader :host, :port, :path
+    def self.red(text='')
+      puts "\e[38;2;255;0;0m#{text}\e[0m"
+    end
 
-  def initialize(host, port, path, secure=false)
-    @host = host
-    @port = port
-    @path = path
-    @secure = secure
+    def self.yellow(text='')
+      puts "\e[38;2;255;255;0m#{text}\e[0m"
+    end
   end
 
-  def secure?
-    @secure
+  class App
+    def initialize(config)
+      @config = config
+    end
+
+    def start
+      req = Net::HTTP.new(@config.url.host, @config.url.port)
+      req.use_ssl = true if @config.url.secure?
+      req.open_timeout = 5
+
+      loop do
+        res = req.get @config.url.path
+        text = "#{@config.url} -> #{res.code} #{res.message}"
+        case res.code.to_i
+        when 100..199
+          Print.yellow(text)
+        when 200..299
+          Print.green(text)
+        when 300..399
+          Print.yellow(text)
+        else
+          Print.red(text)
+        end
+        sleep @config.refresh_interval
+      rescue Errno::ECONNREFUSED
+        Print.red "#{@config.url} -> Connection refused"
+        sleep @config.refresh_interval
+      end
+
+    rescue Interrupt
+      Print.normal
+    end
   end
 
-  def to_s
-    scheme = secure? ? "https" : "http"
-    "#{scheme}://#{host}:#{port}#{path}"
-  end
-end
+  class Url
+    attr_reader :host, :port, :path
 
-class Config
-  attr_reader :url, :refresh_interval
+    def initialize(host, port, path, secure=false)
+      @host = host
+      @port = port
+      @path = path
+      @secure = secure
+    end
 
-  def initialize(argv)
-    @argv = argv
+    def secure?
+      @secure
+    end
 
-    @url = parse_url
-    @refresh_interval = parse_refresh_interval
-  end
-
-  private
-  def parse_url
-    raise HealthMonitor::UrlNotFound.new unless @argv[0]
-    uri = URI(@argv[0])
-    raise HealthMonitor::UrlNotValid.new unless uri.kind_of?(URI::HTTP)
-    path = uri.path.empty? ? '/' : uri.path
-    secure = uri.scheme == 'https'
-    Url.new(uri.host, uri.port, path, secure)
+    def to_s
+      scheme = secure? ? "https" : "http"
+      "#{scheme}://#{host}:#{port}#{path}"
+    end
   end
 
-  def parse_refresh_interval
-    arg_index = @argv.find_index { |arg| arg.start_with?('--refresh_interval=') }
-    return 2 unless arg_index
-    arg_value = @argv[arg_index].split('=')[1]
-    return 2 unless arg_value
-    @argv.delete_at(arg_index)
-    arg_value.to_i
+  class Config
+    attr_reader :url, :refresh_interval
+
+    def initialize(argv)
+      @argv = argv
+
+      @url = parse_url
+      @refresh_interval = parse_refresh_interval
+    end
+
+    private
+    def parse_url
+      raise Exceptions::UrlNotFound.new unless @argv[0]
+      uri = URI(@argv[0])
+      raise Exceptions::UrlNotValid.new unless uri.kind_of?(URI::HTTP)
+      path = uri.path.empty? ? '/' : uri.path
+      secure = uri.scheme == 'https'
+      Url.new(uri.host, uri.port, path, secure)
+    end
+
+    def parse_refresh_interval
+      arg_index = @argv.find_index { |arg| arg.start_with?('--refresh_interval=') }
+      return 2 unless arg_index
+      arg_value = @argv[arg_index].split('=')[1]
+      return 2 unless arg_value
+      @argv.delete_at(arg_index)
+      arg_value.to_i
+    end
   end
 end
 
 if ARGV[0] == '--help'
-  puts "ruby #{$0} [url] --refresh_interval=[refresh_interval]"
+  HealthMonitor::Print.normal "ruby #{$0} [url] --refresh_interval=[refresh_interval]"
 else
-  config = Config.new(ARGV)
-  App.new(config.url, config.refresh_interval).start
+  HealthMonitor::App.new(HealthMonitor::Config.new(ARGV)).start
 end
